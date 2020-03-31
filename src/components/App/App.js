725858f74a6fcd6be6b0c8961e9294b7518cec86
MIGHOST <mi.sidorenko@gmail.com>
2020-04-01 00:01:16 +0300

    added_format_fixed_hw_2_phonebook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,87 +1,71 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component} from 'react';
 import ContactForm from '../ContactForm/ContactForm';
-import shortid from "shortid"
+import shortid from 'shortid';
 import ContactList from '../ContactList/ContactList';
 import ContactFilter from '../ContactFilter/ContactFilter';
-
-
-const filterContact = (contacts, filter) =>
- contacts.filter(contact=> 
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-
-const findContact = (contacts, contact) =>
- contacts.find(item => item.name === contact.name);
+import findContact from '../helpers/findContact';
+import filterContact from '../helpers/filterContact';
 
 export default class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: ''
-  
-  }
+    filter: '',
+  };
 
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
 
-changeFilter = e => {
-  this.setState({filter: e.target.value})
-}
+  addContact = contact => {
+    const { contacts } = this.state;
+    const id = shortid.generate();
+    const savingContact = {
+      id,
+      ...contact,
+    };
+    const oldContacts = this.state.contacts;
+    const foundContact = findContact(contacts, contact);
 
-addContact = contact => {
- const {contacts} = this.state
- const id = shortid.generate();
- const savingContact = {
-  id, 
-  ...contact
-   };
- const oldContacts = this.state.contacts;
- const foundContact = findContact(contacts, contact)
+    if (foundContact) {
+      alert(`${contact.name} is already in contacts!`);
+      return;
+    }
+    if (contact.name.length > 1 && contact.number.length > 1) {
+      this.setState(state => ({ contacts: [...oldContacts, savingContact] }));
+    } else if (contact.name.length <= 1) {
+      alert('Contact name is too small!');
+    } else if (contact.number.length <= 1) {
+      alert('Contact number is too small!');
+    }
+  };
 
- if(foundContact){
-  alert(`${contact.name} is already in contacts!`);
-  return;
- } 
-if (contact.name.length >1 && contact.number.length > 1) {
-  this.setState(state=>({ contacts: [...oldContacts, savingContact] 
-  }));
-
-} else if(contact.name.length <=1){
-  alert("Contact name is too small!");
- 
-} else if (contact.number.length <=1) {
-  alert("Contact number is too small!");
- 
-}
-  
- 
-}
-  
-deleteContact = id => {
-   this.setState( state=> ({
-     contacts:state.contacts.filter(contact=>contact.id !== id)     
-   }));
-};
-
+  deleteContact = id => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(contact => contact.id !== id),
+    }));
+  };
 
   render() {
-    const {contacts, filter} = this.state;
+    const { contacts, filter } = this.state;
     const filteredContacts = filterContact(contacts, filter);
     return (
-      <Fragment> 
-      <h1>Phonebook</h1>
-      <ContactForm onHandleSubmit = {this.addContact}     
-      />
-      <h2>Contacts</h2>
-      <ContactFilter value={filter} onChangeFilter={this.changeFilter}/>
-      {contacts.length >=2 && (
-      <ContactList items={filteredContacts} onDeleteContact={this.deleteContact}/>
-      )}
-      </Fragment>
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onHandleSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactFilter value={filter} onChangeFilter={this.changeFilter} />
+        {contacts.length >= 2 && (
+          <ContactList
+            items={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        )}
+      </>
     );
   }
 }
-
